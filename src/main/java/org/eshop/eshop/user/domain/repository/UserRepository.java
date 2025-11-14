9b5b4d961b67f407b2fa97325d0090cab5b14b65
Malu Abrami <marialuizaabrami@gmail.com>
2025-11-14 20:27:52 -0300

    feat: add interface userRepository da camada da domain que será a "porta" para os outros dominios

---
+++ src/main/java/org/eshop/eshop/user/domain/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package org.eshop.eshop.user.domain.repository;
+
+import org.eshop.eshop.user.domain.entities.User;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository {
+    User save(User user);
+    Optional<User> findById(UUID id);
+    Optional<User> findByEmail(String email);
+}
