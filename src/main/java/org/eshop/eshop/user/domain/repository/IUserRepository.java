d070997b3a1fc3b1876a8d4806f81e07133473b1
Malu Abrami <marialuizaabrami@gmail.com>
2025-11-14 20:58:57 -0300

    fix: ajustando nomenclatura das interfaces

--- src/main/java/org/eshop/eshop/user/domain/repository/IUserRepository.java
+++ src/main/java/org/eshop/eshop/user/domain/repository/IUserRepository.java
@@ -5,7 +5,7 @@ import org.eshop.eshop.user.domain.entities.User;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface UserRepository {
+public interface IUserRepository {
     User save(User user);
     Optional<User> findById(UUID id);
     Optional<User> findByEmail(String email);
--- src/main/java/org/eshop/eshop/user/infra/jpa/IUserJpaRepository.java
+++ src/main/java/org/eshop/eshop/user/infra/jpa/IUserJpaRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface UserJpaRepository extends JpaRepository<UserJpaEntity, UUID>
+public interface IUserJpaRepository extends JpaRepository<UserJpaEntity, UUID>
 {
     Optional<UserJpaEntity> findUserJpaEntitiesByEmail(String email);
 }
